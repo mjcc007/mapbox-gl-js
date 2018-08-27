b3c5e7a8782020e558eba6dea163bc97d79f6523
Janezhangz <42409799+Janezhangz@users.noreply.github.com>
2018-08-27 20:52:05 +0800

    Translate down 100%

--- docs/pages/example/timeline-animation.js
+++ docs/pages/example/timeline-animation.js
@@ -1,11 +1,10 @@
 /*---
-title: Create a time slider
+title: 创建时间滚动条
 description: >-
-  Drag the range slider to visualize earthquakes that were greater than 5.9
-  magnitude in 2015.
+  通过滑动滚动条能够可视化展现发生在2015年大于5.9级的地震.
 tags:
-  - user-interaction
-  - sources
+  - 用户交互
+  - 来源
 pathname: /mapbox-gl-js/example/timeline-animation/
 ---*/
 import Example from '../../components/example';
