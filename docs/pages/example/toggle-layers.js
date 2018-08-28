d1e6ccb1fed3c1ae9dff848e2c777f059f03d877
Janezhangz <42409799+Janezhangz@users.noreply.github.com>
2018-08-28 17:45:30 +0800

    Translate down 100%
    line 2 to line 5

--- docs/pages/example/toggle-layers.js
+++ docs/pages/example/toggle-layers.js
@@ -1,8 +1,8 @@
 /*---
-title: Show and hide layers
-description: Create a custom layer switcher to display different datasets.
+title: 显示和隐藏图层
+description: 创建一个自定义的图层切换器来显示不同的数据集
 tags:
-  - layers
+  - 图层
 pathname: /mapbox-gl-js/example/toggle-layers/
 ---*/
 import Example from '../../components/example';
