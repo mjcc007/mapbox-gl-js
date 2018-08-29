54b10dafc98289225666118f3f958744aceb8251
Jing <jing.liu@mapbox.com>
2018-08-30 00:34:25 +0800

    Update add-image.js

--- docs/pages/example/add-image.js
+++ docs/pages/example/add-image.js
@@ -1,6 +1,6 @@
 /*---
-title: Add an icon to the map
-description: Add an icon to the map from an external URL and use it in a [symbol layer](/mapbox-gl-js/style-spec#layers-symbol).
+title: 在地图上添加一个图标
+description: 从外部URL向地图添加图标，并在 [symbol layer](/mapbox-gl-js/style-spec#layers-symbol)中使用。
 tags:
   - styles
   - layers
