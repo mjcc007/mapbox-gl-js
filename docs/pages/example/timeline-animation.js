b54e3fcfb6664217ed8306dd6ca4494069f3082b
Jing <jing.liu@mapbox.com>
2018-08-29 12:50:52 +0800

    Update timeline-animation.js

--- docs/pages/example/timeline-animation.js
+++ docs/pages/example/timeline-animation.js
@@ -4,8 +4,8 @@ description: >-
   通过滑动滚动条能够可视化展现发生在2015年的大于5.9级的地震.
   
 tags:
-  - 用户交互
-  - 来源
+  - user-interaction
+  - sources
 pathname: /mapbox-gl-js/example/timeline-animation/
 ---*/
 import Example from '../../components/example';
