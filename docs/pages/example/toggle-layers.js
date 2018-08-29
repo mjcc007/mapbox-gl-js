6eb2833f4718cff892de9542e4469d070785da8d
Jing <jing.liu@mapbox.com>
2018-08-29 12:51:11 +0800

    Update toggle-layers.js

--- docs/pages/example/toggle-layers.js
+++ docs/pages/example/toggle-layers.js
@@ -2,7 +2,7 @@
 title: 显示和隐藏图层
 description: 创建一个自定义的图层切换器来显示不同的数据集
 tags:
-  - 图层
+  - layers
 pathname: /mapbox-gl-js/example/toggle-layers/
 ---*/
 import Example from '../../components/example';
