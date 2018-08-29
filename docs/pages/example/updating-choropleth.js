1d4fe4f1db554a43ec8b5dcb5faba423245f46f8
Jing <jing.liu@mapbox.com>
2018-08-29 12:51:24 +0800

    Update updating-choropleth.js

--- docs/pages/example/updating-choropleth.js
+++ docs/pages/example/updating-choropleth.js
@@ -3,8 +3,8 @@ title: 通过不同缩放级别更新专题图图层
 description: >-
   依据不同的缩放等级，展现2014年各国各区县的人口普查数据
 tags:
-  - 图层
-  - 用户交互
+  - layers
+  - user-interaction
 pathname: /mapbox-gl-js/example/updating-choropleth/
 ---*/
 import Example from '../../components/example';
