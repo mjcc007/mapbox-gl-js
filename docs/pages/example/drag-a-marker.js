f95318561815f832552849578ac779059a2c7953
Jing <jing.liu@mapbox.com>
2018-08-30 00:41:23 +0800

    Update drag-a-marker.js

--- docs/pages/example/drag-a-marker.js
+++ docs/pages/example/drag-a-marker.js
@@ -1,8 +1,8 @@
 /*---
-title: Create a draggable Marker
+title: 创建一个可拖动的标记
 description: >-
-  Drag the [`Marker`](/mapbox-gl-js/api/#marker) to a new location on a map and
-  populates its coordinates in a display.
+  将[`Marker`]（/mapbox-gl-js/api/＃marker）拖动到地图上的新位置
+  在显示中填充其坐标。
 tags:
   - user-interaction
 pathname: /mapbox-gl-js/example/drag-a-marker/
